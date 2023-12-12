6f47a9932cdd96e3575d736ab283e7379474337d
Vosida87 <vova.izmodenov@mail.ru>
2023-12-12 15:34:32 +0500

    add create order

--- shop_backend/orders/views.py
+++ shop_backend/orders/views.py
@@ -1,8 +1,17 @@
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
+from common.views import CommonMixin
 from orders.forms import OrderForm
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(CommonMixin, CreateView):
+    """Представление для создания заказа"""
     template_name = 'orders/order-create.html'
     form_class = OrderForm
+    success_url = reverse_lazy('orders:create')
+    title = 'Store - оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
